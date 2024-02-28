313b20fc4ca357bc53a4a3b7f23de007748b064c
Marcio <carvalhorrmarcio@gmail.com>
2024-02-28 00:58:02 -0300

    atualizacao 28-02

--- src/Models/arquivo_preprocessado.py
+++ src/Models/arquivo_preprocessado.py
@@ -3,22 +3,28 @@ from sklearn.model_selection import train_test_split
 import warnings
 warnings.filterwarnings ("ignore")
 
-
+# Criar uma função de imputação
+def imputar(df):
+    if df['Number_Weeks_Used'].isnull().any():
+        df['Number_Weeks_Used'] = df['Number_Weeks_Used'].fillna(df['Number_Weeks_Used'].mean())
+    return df
 
 def preprocessing():
 
    train_d = pd.read_csv("../../Dados/treino.csv")
+   
+# Aplicar a função de imputação usando apply
+   df = train_d.groupby('Crop_Damage', group_keys=False).apply(imputar)
 
-   train_d.drop ('ID', axis=1,inplace=True)
-   train_d.head()
+   df.drop ('ID', axis=1,inplace=True)
 
-   train_d.dropna(inplace=True)
+   df.dropna(inplace=True)
 
    for col in ['Crop_Type','Soil_Type','Pesticide_Use_Category','Season']:
-    train_d = pd.get_dummies(train_d,columns=[col])
+    df = pd.get_dummies(df,columns=[col])
     
-   X = train_d.drop(['Crop_Damage'],axis=1)
-   y = train_d['Crop_Damage'].values.reshape(-1,1)
+   X = df.drop(['Crop_Damage'],axis=1)
+   y = df['Crop_Damage'].values.reshape(-1,1)
 
    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=20, stratify=y)
    return X_train, X_test, y_train, y_test
